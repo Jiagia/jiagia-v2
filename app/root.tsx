e804323057f98a40f1c4728c3092a251d0caf3d8
TrangNDo <trangntdo47@gmail.com>
2025-07-30 17:53:00 -0500

    Change the footer form

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import {useNonce, getShopAnalytics, Analytics} from '@shopify/hydrogen';
+import {useNonce, getShopAnalytics, Analytics, Script} from '@shopify/hydrogen';
 import {type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {
   Link,
@@ -171,6 +171,10 @@ export function Layout({children}: {children?: React.ReactNode}) {
         ) : (
           children
         )}
+        <Script async type="text/javascript" 
+          // src="//static.klaviyo.com/onsite/js/QRiSq4/klaviyo.js" 
+          src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=QRiSq4"
+        />
         <ScrollRestoration nonce={nonce} />
         <Scripts nonce={nonce} />
       </body>
